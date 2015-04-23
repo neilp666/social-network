8325050b05b280693806276605fe6348fe72ebc6
neil patel <neil@myindianweddingapp.com>
2015-04-23 10:06:16 +0100

    Added seeds.db file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "neil1", email: "neilp1@hotmail.co.uk", password: "hello1234", password_confirmation: "hello1234")
+User.create(username: "neil2", email: "neilp2@hotmail.co.uk", password: "hello1234", password_confirmation: "hello1234")
+User.create(username: "neil3", email: "neilp3@hotmail.co.uk", password: "hello1234", password_confirmation: "hello1234")
+User.create(username: "neil4", email: "neilp4@hotmail.co.uk", password: "hello1234", password_confirmation: "hello1234")
+User.create(username: "neil5", email: "neilp5@hotmail.co.uk", password: "hello1234", password_confirmation: "hello1234")
+
+p ("Test users created")
